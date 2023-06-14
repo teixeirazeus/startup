4e1b1907d599f4d9c13e48f31858736b8d1e1e20
teixeirazeus <teixeira.zeus@gmail.com>
2023-06-14 17:31:42 -0300

    init

---
+++ pokemon1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="pokemon1" tilewidth="16" tileheight="16" tilecount="504" columns="18">
+ <image source="../../Downloads/Game Boy Advance - Pokemon FireRed LeafGreen - Tileset.png" trans="ff00ff" width="300" height="450"/>
+</tileset>
